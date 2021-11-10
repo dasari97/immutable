5aba7a4df014f8ca6ece5b3324f60d7a9232db27
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-10 13:58:28 +0000

    adding new file

---
+++ app-module/asg.tf
@@ -0,0 +1,19 @@
+resource "aws_autoscaling_group" "asg" {
+  depends_on                = 
+  name                      = "${var.component}-${var.env}-asg"
+  max_size                  = 1
+  min_size                  = 1
+  desired_capacity          = 1
+  vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  
+  launch_template   {
+      id    =   aws_launch_template.template.id
+      varsion = "$Latest"
+  }
+  
+  tag {
+    key                 = "Name"
+    value               = "${var.component}-${var.env}-asg"
+    propagate_at_launch = true
+  }
+}
