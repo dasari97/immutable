bee67cd01133846500c3c2df7f4661cf2a46c91e
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-11 10:57:50 +0000

    adding new file

--- app-module/data.tf
+++ app-module/data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "${var.component}-1*"
+  name_regex       = "${var.component}-${var.APP_VERSION}"
   owners           = ["self"]
   }
   
