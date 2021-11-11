08b9137b1b0d1a1fee3973378eba0319ee48c314
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-11 07:19:02 +0000

    adding new file

--- Databases/data.tf
+++ Databases/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "^Centos-7*"
-  owners           = ["973714476881"]
+  name_regex       = "^ansilbe"
+  owners           = ["self"]
   }
   
 data "terraform_remote_state" "vpc" {
